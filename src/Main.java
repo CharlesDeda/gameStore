72935e8410d9a9bbe22b7776bab7ee841dad8c48
CharlesDeda <dedaplays@yahoo.com>
2024-02-19 08:19:54 -0500

    wip

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Random;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        String[] games = {
+                "Betrayal at House on the Hill", "Crokinole", "Cthulu Dice", "Doom",
+                "Dungeons & Dragons", "Escape: Curse of the Temple",
+                "Everdell", "Fluxx", "Hollow Knight", "Loonacy", "Love Letter", "Mario Kart",
+                "Mysterium", "Night Cage", "Pathfinder", "Play Me", "Poetry for Neanderthals", "Roll for It", "Root",
+                "Stardew Valley", "Sushi Go", "Tetris", "Ticket To Ride", "Warhammer", "World of Warcraft", "Zombie Dice"
+        };
+        boolean[] alreadyChosen = new boolean[games.length];
+        int count = 0;
+        while(count < 12) {
+            Random r = new Random();
+            int randomNumber= r.nextInt(0, games.length);
+            if (alreadyChosen[randomNumber] == true) {
+                randomNumber = r.nextInt(0,games.length);
+            } else if(alreadyChosen[randomNumber] == false) {
+                alreadyChosen[randomNumber] = true;
+                System.out.println(games[randomNumber]);
+                count += 1;
+                //asdf
+            }
+
+        }
+
+        }
+    }
