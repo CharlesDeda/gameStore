65fbd76fcb6a18a43d28d5e5b2000f19d70aa97d
CharlesDeda <dedaplays@yahoo.com>
2024-02-19 13:01:52 -0500

    wip

--- src/digitalGame.java
+++ src/digitalGame.java
@@ -2,6 +2,9 @@ public class digitalGame extends Games {
     protected String genre;
 
     public digitalGame(String title, double cost, int playerCount, double time, double rating, String genre) {
-        super(title, cost, playerCount, time, rating, genre);
+        super(title, cost, playerCount, time, rating);
+        this.genre = genre;
     }
+    private String getGenre() {return genre;}
+    public String setGenre(String genre) {return this.genre = genre;}
 }
--- src/physicalGame.java
+++ src/physicalGame.java
@@ -1,6 +1,9 @@
 public class physicalGame extends Games {
     protected String genre;
     public physicalGame(String title, double cost, int playerCount, double time, double rating, String genre) {
-        super(title, cost, playerCount, time, rating, genre);
+        super(title, cost, playerCount, time, rating);
+        this.genre = genre;
     }
+    private String getGenre() {return genre;}
+    public String setGenre(String genre) {return this.genre = genre;}
 }
