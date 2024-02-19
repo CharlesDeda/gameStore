115267a750c1760d9162ecbf06b82dcc36385c4c
CharlesDeda <dedaplays@yahoo.com>
2024-02-19 12:46:11 -0500

    constructor, instances,

--- src/Games.java
+++ src/Games.java
@@ -1,4 +1,52 @@
 public class Games {
     private String title;
-    
+    private double cost;
+    private int playerCount;
+
+    private double time;
+    private double rating;
+
+    public Games(String title, double cost, int playerCount, double time, double rating) {
+        this.title = title;
+        this.cost = cost;
+        this.playerCount = playerCount;
+        this.time = time;
+        this.rating = rating;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+    public int getPlayerCount() {
+        return playerCount;
+    }
+    public double getTime() {
+        return time;
+    }
+    public double getRating() {
+        return rating;
+    }
+
+    public String setTitle(String title) {
+        return this.title = title;
+    }
+    public String setGenre(String genre) {
+        return this.genre = genre;
+    }
+    public double setCost(double cost) {
+        return this.cost = cost;
+    }
+    public int setPlayerCount(String title) {
+        return this.playerCount = playerCount;
+    }
+    public double setTime(double time) {
+        return this.time = time;
+    }
+    public double setRating(double rating) {
+        return this.rating = rating;
+    }
 }
