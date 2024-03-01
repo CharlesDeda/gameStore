4059c50c894c28adbf1f354ae59d6711adb169b7
CharlesDeda <dedaplays@yahoo.com>
2024-03-01 11:48:14 -0500

    wip

--- src/WorldOfWarcraft.java
+++ src/WorldOfWarcraft.java
@@ -1,5 +1,9 @@
 public class WorldOfWarcraft extends digitalGame {
-    public WorldOfWarcraft(String title, double cost, double time, double rating, String physicalType, int party) {
+    private String race;
+    private String class;
+    public WorldOfWarcraft(String title, double cost, double time, double rating, String physicalType, int party, String race, String class) {
         super(title, cost, time, rating, physicalType, party);
+        this.race = race;
+        this.class = class
     }
 }
