ac981ad86cded6375b4bf0e44511bc549276d06a
CharlesDeda <dedaplays@yahoo.com>
2024-03-13 11:50:58 -0400

    added all Joshua files to gamesBranch

---
+++ src/MarioKart.java
@@ -0,0 +1,25 @@
+public class MarioKart extends digitalGame{
+    //I am going to be referencing the WII game version for easy of use
+    private String character;
+    private String kart;
+    private String format;
+    //The format for this would be a two or three option question. Battle(VS), or race(GrandPre). (can be more specific)
+    private String track;
+    //which track/map is to be played on
+
+    public MarioKart(String title, double cost, double time, double rating, String physicalType, int party) {
+        super(title, cost, time, rating, physicalType, party);
+        this.character = character;
+        this.kart = kart;
+        this.format = format;
+        this.track = track;
+    }
+    public String getCharacter() {return character;}
+    public String getKart() {return kart;}
+    public String getFormat() {return format;}
+    public String getTrack() {return track;}
+    public String setCharacter(String character) {return this.character=character;}
+    public String setKart(String kart) { return this.kart=kart;}
+    public String setFormat(String format) { return this.format=format;}
+    public String setTrack(String track) { return this.track= track;}
+}
