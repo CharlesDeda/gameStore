fa1728d4600fdcd222901efa108b1cd0e722bee1
CharlesDeda <dedaplays@yahoo.com>
2024-03-13 12:46:25 -0400

    loonacy

---
+++ src/Loonacy.java
@@ -0,0 +1,24 @@
+/**
+ *
+ * @desc Creates the Loonacy game class and it also adds a
+ * FreeForAll variable which allows you to set the game as a free for all
+ */
+public class Loonacy extends physicalGame {
+    private Boolean FreeForAll;
+
+    // the Loonacy constructor using the super class constructor as well
+    public Loonacy(String title, double cost, double time, double rating, String physicalType,
+                   int playerCount, Boolean FreeForAll) {
+        super(title, cost, time, rating, physicalType, playerCount);
+        this.FreeForAll = FreeForAll;
+    }
+    // getters and setters
+    private Boolean getFreeForAll() {return FreeForAll;}
+    public void setFreeForAll(Boolean FreeForAll) {this.FreeForAll = FreeForAll;}
+
+    // toString method
+    public String toString() {
+        return super.toString() + String.format("%-10b",
+                getFreeForAll());
+    }
+}
