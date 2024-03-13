70703c2a584995431aee144ce7a0950c5adcfe96
Justin-Underscore <160261544+Justin-Underscore@users.noreply.github.com>
2024-03-13 13:02:10 -0400

    WIP
    Initial Games

--- src/Main.java
+++ src/Main.java
@@ -19,12 +19,16 @@ public class Main {
         int copies = input.nextInt();
 
 
-//        WorldOfWarcraft w1 = new WorldOfWarcraft("World of Warcraft", 50, 2707,
+//        WorldOfWarcraft w1 = new WorldOfWarcraft("World of Warcraft", 50.00, 2707,
 //                10, "MMO", 4, "Panda");
 //        System.out.println(w1);
-//        Everdell e1 = new Everdell("Everdell", 20, 2707, 8.5, "Boardgame", 4, "Construction");
+//        Everdell e1 = new Everdell("Everdell", 20.00, 2707, 8.5, "Boardgame", 4, "Construction");
 //        System.out.println(e1);
-//        PoetryForNeanderthals p1 = new PoetryForNeanderthals("Poetry For Neanderthals", 30, 45, 7.3, "Boardgame", 3, "NewCard");
+//        PoetryForNeanderthals p1 = new PoetryForNeanderthals("Poetry For Neanderthals", 30.00, 45, 7.3, "Boardgame", 3, "NewCard");
 //        System.out.println(p1);
+//        BetrayalAtHouseOnTheHill b1 = new BetrayalAtHouseOnTheHill("Betrayal at House on The Hill",
+//                40.00, "30 minutes to an hour", 9, "Boardgame", 6, "Player 1");
+//        Fluxx f1 = new Fluxx("Fluxx", 20.00, "5 to 30 minutes", 7, "Cardgame", 6, "Cthulu Fluxx");
+//        Loonacy l1 = new Loonacy("Loonacy", 20.00, "5 to 10 minutes", 10, "Cardgame", 5, Boolean.TRUE);
     }
 }
