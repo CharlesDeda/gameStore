1cc32c29cdc28047e4f0e1e2021373b7e809eaf5
CharlesDeda <dedaplays@yahoo.com>
2024-03-13 12:45:43 -0400

    created newGame

---
+++ src/BetrayelAtHouseOnTheHill.java
@@ -0,0 +1,24 @@
+/**
+ *
+ * @desc Creates the BetrayalAtHouseOnTheHill game class and it also adds a
+ * Betrayer variable which allows you to input who the Betrayer is
+ */
+public class BetrayalAtHouseOnTheHill extends physicalGame {
+    private String Betrayer;
+
+    // the BetrayalAtHouseOnTheHill constructor using the super class constructor as well
+    public BetrayalAtHouseOnTheHill(String title, double cost, double time, double rating, String physicalType,
+                                    int playerCount, String Betrayer) {
+        super(title, cost, time, rating, physicalType, playerCount);
+        this.Betrayer = Betrayer;
+    }
+    // getters and setters
+    private String getBetrayer() {return Betrayer;}
+    public void setBetrayer(String Betrayer) {this.Betrayer = Betrayer;}
+
+    // toString method
+    public String toString() {
+        return super.toString() + String.format("%-10s",
+                getBetrayer());
+    }
+}
