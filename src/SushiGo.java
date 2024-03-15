4d1ea28e84d296190a0613c4d44bc6cb158ded02
Justin-Underscore <160261544+Justin-Underscore@users.noreply.github.com>
2024-03-15 13:11:06 -0400

    finished
    100% :)

--- src/SushiGo.java
+++ src/SushiGo.java
@@ -1,7 +1,6 @@
 public class SushiGo extends physicalGame{
     //This is a card game much like spoons, where you try to get the best combination of cards before your opponents but there is a point system.
     private String hand;
-    //Type of cards or amount in starting hand? (7)
     //private String cards
 
     public SushiGo(String title, double cost, String time, double rating, String physicalType, int playerCount, String hand) {
